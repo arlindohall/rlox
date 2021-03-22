a83c89e09d682fdc41bff7b88c7c755457c32bc4
Miller Hall <hall@hey.com>
2021-03-22 16:17:42 -0400

    Implement real timing logic

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,12 +1,16 @@
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use crate::{interpreter::{LoxCallable}, lox::LoxError, parser::LoxObject};
 
-
-
 /*
- * Built-in clock function
+ * Built-in clock function. We deviate from lox and show miliseconds.
  */
-fn clock_impl(args: Vec<LoxObject>) -> Result<LoxObject, LoxError> {
-    Ok(LoxObject::Number(0.0))
+fn clock_impl(_args: Vec<LoxObject>) -> Result<LoxObject, LoxError> {
+    match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(time) => Ok(LoxObject::Number(time.as_millis() as f64)),
+        Err(_err) => todo!(), // TODO: how to handle legit system time error?
+    }
 }
 
 pub fn clock() -> LoxObject {
